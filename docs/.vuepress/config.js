e4e994c165511fa8946e3969330cdb9793bb2e1a
谢东磊 <344919627@qq.com>
2018-05-08 23:22:14 +0800

    :tada: install vuepress and static html removed

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dest: "dist",
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "ServerHub MVC",
+      description: "Fast and reliable MVC framework for Nodejs"
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "ServerHub MVC",
+      description: "快速可靠的 Nodejs MVC 框架"
+    }
+  }
+};
